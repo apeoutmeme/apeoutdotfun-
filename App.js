76d2ec88a11e944377e50a2375ac027d4846d019
apeoutmeme <apeoutcrypto@gmail.com>
2024-10-26 06:39:35 -0700

    Update App.js
    The useEffect hook for initializing favorites from App.js

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ const AppWrapper = () => {
   const network = '' || clusterApiUrl('mainnet-beta');
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    setFavorites(storedFavorites);
+    const storedBuyAmount = localStorage.getItem('buyAmount') || 'custom';
+    setBuyAmount(storedBuyAmount);
+    const storedBoughtTokens = JSON.parse(localStorage.getItem('boughtTokens')) || [];
+    setBoughtTokens(storedBoughtTokens);
+  }, []);
+
   return (
     <ConnectionProvider endpoint={network}>
     <WalletProvider wallets={wallets} autoConnect>
